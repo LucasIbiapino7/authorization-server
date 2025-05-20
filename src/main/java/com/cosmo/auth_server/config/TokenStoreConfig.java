37f1bc2ab97c3e14c54755a340b42c96bcfc70f5
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2025-05-20 16:25:51 -0300

    feat: add token customizer with claims

--- src/main/java/com/cosmo/auth_server/config/TokenStoreConfig.java
+++ src/main/java/com/cosmo/auth_server/config/TokenStoreConfig.java
@@ -8,15 +8,20 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -40,6 +45,25 @@ public class TokenStoreConfig {
     JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
         return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
+
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+        return context -> {
+            if ("access_token".equals(context.getTokenType().getValue())) {
+                UserDetails principal = (UserDetails) context.getPrincipal().getPrincipal();
+                List<String> roles = principal.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList();
+
+                context.getClaims()
+                        .claim("authorities", roles)
+                        .claim("username", principal.getUsername());
+            }
+        };
+    }
+
 
     private static com.nimbusds.jose.jwk.RSAKey generateRsa() {
         KeyPair keyPair = generateRsaKey();
